5d4346acdf622b43456285835142c7b44010209a
Christian Taylor <christian.taylor17@my.stjohns.edu>
2020-06-01 00:00:17 -0400

    Test

--- interrupt_testing/test.py
+++ interrupt_testing/test.py
@@ -13,60 +13,74 @@ try:
     import utils.config
 except:
     print(e)
-    
-bus=None
+
+bus = None
+
 
 def speed_adj_down(interupt_pin):
     print("Speed adj down triggered")
 
+
 def speed_adj_up(interupt_pin):
     print("Speed adj up triggered")
+
 
 def speed_adj_default(interupt_pin):
     print("Speed adj default triggered")
 
+
 def brightness_adj_default(interupt_pin):
     print("Brightness adj default triggered")
+
 
 def brightness_adj_up(interupt_pin):
     print("Brightness adj up triggered")
 
+
 def brightness_adj_down(interrupt_pin):
     print("Brightness adj down triggered")
+
 
 def turn_off_lights(interrupt_pin):
     print("Lights off triggered")
 
+
 def turn_on_lights(interrupt_pin):
     print("Lights on triggered")
+
 
 def mode_change_up_button_pressed(interrupt_pin):
     print("Mode change up triggered")
 
+
 def mode_change_down_button_pressed(interrupt_pin):
     print("Mode change down triggered")
 
+
 def default_mode_button_pressed(interrupt_pin):
     print("Mode default triggered")
+
 
 def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
 
+
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    
-    DEFAULT_MODE     = 16
-    MODE_UP          = 12
-    MODE_DOWN        = 1
-    TURN_OFF         = 21
-    TURN_ON          = 20
-    BRIGHTNESS_UP    = 8
-    BRIGHTNESS_DOWN  = 25
-    BRIGHTNESS_DEF   = 7
-    SPEED_ADJ_UP     = 23
-    SPEED_ADJ_DOWN   = 18
-    SPEED_DEF        = 24
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+
+    DEFAULT_MODE = 16
+    MODE_UP = 12
+    MODE_DOWN = 1
+    TURN_OFF = 21
+    TURN_ON = 20
+    BRIGHTNESS_UP = 8
+    BRIGHTNESS_DOWN = 25
+    BRIGHTNESS_DEF = 7
+    SPEED_ADJ_UP = 23
+    SPEED_ADJ_DOWN = 18
+    SPEED_DEF = 24
 
     GPIO.setup(DEFAULT_MODE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(MODE_UP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
